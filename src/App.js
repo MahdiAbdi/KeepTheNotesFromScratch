9bdc0506eae87bdf109fef2fbc56e33f5a425e37
Mahdi Abdi <abdi@inside.sahab.ir>
2019-01-06 20:31:51 +0330

    add firebase. test some firestore features

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: ""
+    };
+  }
+
+  componentDidMount() {
+    const config = {
+      apiKey: "AIzaSyB3GYcRcn40zuiUOL9oEiXC2pE7RWBPX8U",
+      authDomain: "keepthenotes-bf133.firebaseapp.com",
+      databaseURL: "https://keepthenotes-bf133.firebaseio.com",
+      projectId: "keepthenotes-bf133",
+      storageBucket: "keepthenotes-bf133.appspot.com",
+      messagingSenderId: "682012368542"
+    };
+
+    firebase.initializeApp(config);
+
+    const db = firebase.firestore();
+    db.settings({ timestampsInSnapshots: true });
+
+    (async function getNotes() {
+      const notes = await db.collection("notes").get();
+
+      notes.docs.map(note => console.log(note.data()));
+    })();
+  }
+
+  autoSave(e) {
+    this.setState({ content: e.target.value });
+    const db = firebase.firestore();
+    db.collection('notes').add({
+      content: this.state.content,
+      author: 'Mahdi',
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -20,6 +60,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <textarea value={this.state.content} onChange={this.autoSave.bind(this)} />
       </div>
     );
   }
