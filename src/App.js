0d2c47ef15609985da5a8dc7a984803861979033
Mahdi Abdi <abdi@inside.sahab.ir>
2019-01-09 20:34:45 +0330

    Functionality Complete :D B)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import firebase from "firebase/app";
+import firebase from "firebase";
 import "firebase/firestore";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      content: ""
+      docId: null,
+      note: "",
+      token: null,
+      user: null,
+      error: null
     };
   }
+
+  provider = null;
 
   componentDidMount() {
     const config = {
@@ -24,43 +28,77 @@ class App extends Component {
 
     firebase.initializeApp(config);
 
-    const db = firebase.firestore();
-    db.settings({ timestampsInSnapshots: true });
-
-    (async function getNotes() {
-      const notes = await db.collection("notes").get();
-
-      notes.docs.map(note => console.log(note.data()));
-    })();
+    this.provider = new firebase.auth.GoogleAuthProvider();
+    this.provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
+    const auth = firebase.auth();
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ user });
+        const db = firebase.firestore();
+        db.settings({ timestampsInSnapshots: true });
+        db.collection("notes")
+          .where("author", "==", this.state.user.email)
+          .onSnapshot(snapshot => {
+            let changes = snapshot.docChanges();
+            changes.forEach(change => {
+              if (change.type === "added") {
+                this.setState({ docId: change.doc.id });
+              } else if (change.type === "removed") {
+                this.setState({ note: "" });
+              }
+              this.setState({ note: change.doc.data().content });
+            });
+          });
+      } else {
+        this.setState({
+          docId: null,
+          note: "",
+          token: null,
+          user: null,
+          error: null
+        });
+      }
+    });
   }
 
   autoSave(e) {
-    this.setState({ content: e.target.value });
     const db = firebase.firestore();
-    db.collection('notes').add({
-      content: this.state.content,
-      author: 'Mahdi',
-    });
+    if (this.state.docId) {
+      db.collection("notes")
+        .doc(this.state.docId)
+        .update({ content: e.target.value });
+    } else {
+      db.collection("notes").add({
+        author: this.state.user.email,
+        content: e.target.value
+      });
+    }
+  }
+
+  auth(e) {
+    const auth = firebase.auth();
+    if (this.state.user) {
+      auth.signOut();
+    } else {
+      auth
+        .signInWithPopup(this.provider)
+        .then(result => {})
+        .catch(error => {});
+    }
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <textarea value={this.state.content} onChange={this.autoSave.bind(this)} />
+        <button onClick={this.auth.bind(this)}>
+          {this.state.user ? `Hi ${this.state.user.email}! Log Out` : "Log In"}
+        </button>
+        {this.state.user ? (
+          <textarea
+            value={this.state.note}
+            onChange={this.autoSave.bind(this)}
+          />
+        ) : null}
       </div>
     );
   }
